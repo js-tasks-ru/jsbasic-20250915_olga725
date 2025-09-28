1211aecb2fa1306f0ea3dbd8de0ebc42104d462d
unknown <olga.oleynikova@bk.ru>
2025-09-28 18:28:13 +0300

    2-module \ 1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
-}
+  let sum = 0; 
+  for (let k in salaries) { 
+    const zp = salaries[k];
+    if (typeof zp === 'number' && isFinite(zp)) { 
+      sum += zp; 
+    } 
+  } 
+  return sum; 
+};
