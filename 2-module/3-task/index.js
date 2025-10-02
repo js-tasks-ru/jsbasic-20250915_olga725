9577f0d168a3bd56b567754230d4b5998768f19b
unknown <olga.oleynikova@bk.ru>
2025-10-02 18:50:15 +0300

    2-module \ 3-task

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,15 +1,15 @@
 let calculator = {
-  num1: 3,
-  num2: 5,
+  num1: 0,
+  num2: 0,
   read: function(num1,num2) {
-    calculator.num1 = num1;
-    calculator.num2 = num2;
+    this.num1 = num1;
+    this.num2 = num2;
   },
   sum: function() {
-    return calculator.num1 + calculator.num2;
+    return this.num1 + this.num2;
   },
   mul: function() {
-    return calculator.num1 * calculator.num2;
+    return this.num1 * this.num2;
   },
 };
 
