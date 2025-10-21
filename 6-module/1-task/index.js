711e32a59f1b5f9d2fbe7294312b93e9a4661570
unknown <olga.oleynikova@bk.ru>
2025-10-21 19:54:38 +0300

    6-module \ 1-3-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,50 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.render();
+  }
+  render() {
+    const table = document.createElement('table');
+
+    const thead = document.createElement('thead');
+    const theadTr = document.createElement('tr');
+    const headers = ['Имя', 'Возраст', 'Зарплата', 'Город', ''];
+
+    headers.forEach(text => {
+      const th = document.createElement('th');
+      th.textContent = text;
+      theadTr.appendChild(th);
+    });
+    thead.appendChild(theadTr);
+    table.appendChild(thead);
+
+    const tbody = document.createElement('tbody');
+    const createRow = (user) => {
+      const tr = document.createElement('tr');
+      ['name', 'age', 'salary', 'city'].forEach(key => {
+        const td = document.createElement('td');
+        td.textContent = user[key];
+        tr.appendChild(td);
+      });
+
+      const tdDel = document.createElement('td');
+      const btn = document.createElement('button');
+      btn.textContent = 'X';
+      btn.addEventListener('click', () => {
+        tr.remove();
+      });
+      tdDel.appendChild(btn);
+      tr.appendChild(tdDel);
+
+      return tr;
+    };
+
+    this.rows.forEach(user => {
+      tbody.appendChild(createRow(user));
+    });
+
+    table.appendChild(tbody);
+    return table;
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,5 +3,87 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this._container = this._createCarousel();
+    this._currentSlide = 0;
+    this.arrowLeft = this._container.querySelector('.carousel__arrow_left');
+    this.arrowRight = this._container.querySelector('.carousel__arrow_right');
+    this._inner = this._container.querySelector('.carousel__inner');
+    this._createSlides();
+    this._addElementRow();
+    this._updateArrows();
+  }
+
+  get elem() {
+    return this._container;
+  }
+
+  _createCarousel() {
+    const htmlCarusel = `
+      <div class="carousel">
+        <button type="button" class="carousel__arrow carousel__arrow_left">
+          <img src="/assets/images/icons/angle-left-icon.svg" alt="left">
+        </button>
+        <div class="carousel__inner"></div>
+        <button type="button" class="carousel__arrow carousel__arrow_right">
+          <img src="/assets/images/icons/angle-icon.svg" alt="right";>
+        </button>
+      </div>
+    `;
+    const element = createElement(htmlCarusel);
+    return element;
+  }
+  _createSlides() {
+    this.slides.forEach(slide => {
+      const htmlSlides = `
+        <div class="carousel__slide" data-id="${slide.id}">
+          <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
+          <div class="carousel__caption">
+            <span class="carousel__price">€${slide.price.toFixed(2)}</span>
+            <div class="carousel__title">${slide.name}</div>
+            <button type="button" class="carousel__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+            </button>
+          </div>
+        </div>
+      `;
+      const slideElement = createElement(htmlSlides);
+      slideElement.dataset.id = slide.id;
+      const button = slideElement.querySelector('.carousel__button');
+        button.addEventListener('click', () => {
+          this._container.dispatchEvent(new CustomEvent('product-add', {
+            detail: slide.id,
+            bubbles: true,
+          }));
+        });
+      this._inner.appendChild(slideElement);
+    });
+  }
+
+  _addElementRow() {
+    this.arrowLeft.addEventListener('click', () => {
+      if (this._currentSlide > 0) {
+        this._currentSlide--;
+        this._updateCarousel();
+      }
+    });
+    
+    this.arrowRight.addEventListener('click', () => {
+      if (this._currentSlide < this.slides.length - 1) {
+        this._currentSlide++;
+        this._updateCarousel();
+      }
+    });
+  }
+
+  _updateCarousel() {
+    const widthNew = this._container.querySelector('.carousel__inner').offsetWidth;
+    const translateX = this._currentSlide * (-widthNew);
+    this._inner.style.transform = `translateX(${translateX}px)`;
+    this._updateArrows();
+  }
+
+  _updateArrows() {
+    this.arrowLeft.style.display = this._currentSlide === 0 ? 'none' : '';
+    this.arrowRight.style.display = this._currentSlide === this.slides.length - 1 ? 'none' : '';
   }
 }
